7acb5798810c3b6fdfbf63ff3e295c96cfb37ea7
notesservice <wdynotesteam@gmail.com>
2023-01-03 16:50:03 +0800

    a

--- roi.py
+++ roi.py
@@ -6,8 +6,14 @@ import PIL
 from PIL import Image
 
 model = load_model('CNNmodel.h5')
+cam_capture = cv2.VideoCapture(0)
+
+colors = []
+for i in range(0,20):
+    colors.append((245,117,16))
 
 
+window_name = "Sign Language Detection using CNN Algorithmn"
 def prediction(pred):
     return(chr(pred+ 65))
 
@@ -29,23 +35,23 @@ def keras_process_image(img):
  
 
 def crop_image(image, x, y, width, height):
-    return image[y:y + height, x:x + width]
+    # return image[y:y + height, x:x + width]
+    return image[40:400,0:300]
 
 def main():
     l = []
     
     while True:
-        
-        cam_capture = cv2.VideoCapture(0)
         _, image_frame = cam_capture.read()  
     # Select ROI
         im2 = crop_image(image_frame, 300,300,300,300)
+
+        image_frame=cv2.rectangle(image_frame,(0,40),(300,400),255,2)
+
         image_grayscale = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
     
         image_grayscale_blurred = cv2.GaussianBlur(image_grayscale, (15,15), 0)
         im3 = cv2.resize(image_grayscale_blurred, (28,28), interpolation = cv2.INTER_AREA)
-
-
     
         im4 = np.resize(im3, (28, 28, 1))
         im5 = np.expand_dims(im4, axis=0)
@@ -54,27 +60,29 @@ def main():
         pred_probab, pred_class = keras_predict(model, im5)
     
         curr = prediction(pred_class)
-        
-        cv2.putText(image_frame, curr, (700, 300), cv2.FONT_HERSHEY_COMPLEX, 4.0, (255, 255, 255), lineType=cv2.LINE_AA)
-            
-            
-    
+
+        print(pred_probab,curr)
+
+        #cv2.putText(image_frame, curr, (100, 300), cv2.FONT_HERSHEY_COMPLEX, 4.0, (255, 255, 255), lineType=cv2.LINE_AA)
  
     # Display cropped image
-        cv2.rectangle(image_frame, (300, 300), (600, 600), (255, 255, 00), 3)
-        cv2.imshow("frame",image_frame)
+
+        cv2.rectangle(image_frame, (0,0), (300, 40), (245, 117, 16), -1)
+        cv2.putText(image_frame,"Output: - "+' '.join(curr), (3,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        #cv2.rectangle(image_frame, (300, 300), (600, 600), (255, 255, 00), 3)
+        cv2.imshow(window_name, frame)
+        cv2.setWindowProperty(window_name,cv2.WND_PROP_TOPMOST,1)
         
         
     #cv2.imshow("Image4",resized_img)
-        cv2.imshow("Image3",image_grayscale_blurred)
+        # cv2.imshow("Image3",image_grayscale_blurred)
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
+            cv2.destroyAllWindows()
+            break
 
-
-if __name__ == '__main__':
-    main()
+main()
 
 cam_capture.release()
 cv2.destroyAllWindows()
